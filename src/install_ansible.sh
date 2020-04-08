f50119e80aadea37d7a970361b6d1d5c394b89fb
Gypsy Rogers <grogers@nvidia.com>
2020-04-07 20:48:18 -0700

    add ansible dir

--- src/install_ansible.sh
+++ src/install_ansible.sh
@@ -10,7 +10,11 @@ sudo apt install python3.7  -y
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt update
 sudo apt-get install ansible -y
+git clone https://github.com/hightekvagabond/QBSWebServer.git
+mv /home/ubuntu/ansible .
+rm -rf QBSWebServer
 ansible -m ping localhost
+rm /home/ubuntu/install_ansible.sh
 echo "*******EVERYTHING IS INSTALLED*********"
 
 
